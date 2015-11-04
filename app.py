c7bcd76b694514efbd77e052031b3aa188cb5b4f
Axel Goetz <axel.goez@hotmail.com>
2015-11-04 11:53:47 +0000

    Added migrations

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 db = SQLAlchemy(app)
 
+from models import User
+
 @app.route("/")
 def hello():
     return render_template('login.html', title="LOGIN")
