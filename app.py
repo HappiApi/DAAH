33e39cda3fb339ca50eb0505bd3e21f5ee3929f1
heklahelgadottir <heklahelgadottir@gmail.com>
2015-11-04 22:28:45 +0000

    Linking pages
    Still need to tidy the code, especially the CSS part

--- app.py
+++ app.py
@@ -6,7 +6,23 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 
 @app.route("/")
 def hello():
-    return render_template('login.html', title="LOGIN")
+    return render_template('login.html', title="Log In")
+
+@app.route("/signup")
+def signup():
+    return render_template('signup.html', title="Sign Up")
+
+@app.route("/lists")
+def lists():
+    return render_template('lists.html', title="Lists")
+
+@app.route("/newlist")
+def newlist():
+    return render_template('newlist.html', title="New List")
+
+@app.route("/newitem")
+def newitem():
+    return render_template('newitem.html', title="New Item")
 
 if __name__ == "__main__":
     app.run()
