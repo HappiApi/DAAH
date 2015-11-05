f70d399964832a5bc6579e6d7371d86e7cbc81e9
Daniel Gavrilov <dgavrilov95@gmail.com>
2015-11-05 17:18:30 +0000

    Update bootstrap.sh & vagrant

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
+REPO="https://github.com/HappiApi/DAAH.git"
+
 echo Fresh new machine! I like!
 
+# sudo adduser dev
+# sudo passwd dev
+# gpasswd -a dev wheel
+# su dev
+
+sudo yum -y install binutils gcc make patch libgomp glibc-headers glibc-devel kernel-headers kernel-devel git epel-release wget
+
+sudo yum -y install python34 python34-devel
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python3.4 get-pip.py
+
 sudo rpm -Uvh http://yum.postgresql.org/9.4/redhat/rhel-7-x86_64/pgdg-centos94-9.4-1.noarch.rpm
-sudo yum update
-sudo yum install postgresql94-server postgresql94-contrib postgresql94-libs postgresql94-devel
+sudo yum -y update
+sudo yum -y install postgresql94-server postgresql94-contrib postgresql94-libs postgresql94-devel
 sudo /usr/pgsql-9.4/bin/postgresql94-setup initdb
 sudo systemctl start postgresql-9.4
 sudo systemctl enable postgresql-9.4
-git clone https://github.com/HappiApi/DAAH.git daah
+
+git clone $REPO daah
+sudo chown -R `whoami` daah/
 cd daah
+sudo pip3 install virtualenv
 virtualenv venv
 echo "export APP_SETTINGS='config.DevelopmentConfig'" >> venv/bin/activate
 echo "export DATABASE_URL='postgres://postgres@localhost'" >> venv/bin/activate
 source venv/bin/activate
-pip install uwsgi
-pip install -r requirements.txt
-python app.py
+PATH=$PATH:/usr/pgsql-9.4/bin/ pip3 install -r requirements.txt
 
+# pip3 install uwsgi
