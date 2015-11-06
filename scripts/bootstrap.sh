23678e055e494c3e76336b890c5fc27376894ba1
HappiApi <hasthanasombat.a@gmail.com>
2015-11-06 14:28:19 +0000

    Add more automation

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -18,6 +18,9 @@ sudo rpm -Uvh http://yum.postgresql.org/9.4/redhat/rhel-7-x86_64/pgdg-centos94-9
 sudo yum -y update
 sudo yum -y install postgresql94-server postgresql94-contrib postgresql94-libs postgresql94-devel
 sudo /usr/pgsql-9.4/bin/postgresql94-setup initdb
+#copy in pgsql_hba.conf
+cp pg_hba.conf /var/lib/pgsql/9.4/data/pg_hba.conf
+
 sudo systemctl start postgresql-9.4
 sudo systemctl enable postgresql-9.4
 
@@ -27,9 +30,12 @@ cd daah
 sudo pip3 install virtualenv
 virtualenv venv
 echo "export APP_SETTINGS='config.DevelopmentConfig'" >> venv/bin/activate
-echo "export DATABASE_URL='postgres://postgres@localhost'" >> venv/bin/activate
+echo "export DATABASE_URL='postgres://localuser@localhost/postgres'" >> venv/bin/activate
 source venv/bin/activate
 PATH=$PATH:/usr/pgsql-9.4/bin/ pip3 install -r requirements.txt
+
+#upgrade database
+python manage.py db upgrade
 
 # removing the old postgres version
 deactivate
@@ -37,13 +43,24 @@ sudo yum remove postgresql
 echo "export PATH=\$PATH:/usr/pgsql-9.4/bin/" >> ~/.bash_profile
 source ~/.bash_profile
 
-sudo /bin/bash && sudo -u postgres psql postgres -c "CREATE USER localuser WITH PASSWORD 'localuser'; GRANT ALL PRIVILEGES ON DATABASE postgres TO localuser; ALTER DATABASE postgres OWNER TO localuser; "
-source venv/bin/activate
+sudo /bin/bash << EOF
+sudo -u postgres psql postgres -c "CREATE USER localuser WITH PASSWORD 'localuser'; GRANT ALL PRIVILEGES ON DATABASE postgres TO localuser; ALTER DATABASE postgres OWNER TO localuser; "
+EOF
 
-# schedule backup
+# schedule backup for 1 AM
 sudo crontab -l > mycron
 echo "0 1 * * * bash ~/DAAH/scripts/backup.sh" >> mycron
 crontab mycron
 rm mycron
 
-# pip3 install uwsgi
+#start the server 
+source venv/bin/activate
+gunicorn --bind 127.0.0.1:8080 wsgi:app &
+#copy in nginx conf file
+cp nginx.conf /etc/nginx/nginx.conf
+sudo systemctl enable nginx
+sudo systemctl start nginx
+
+echo Pray for the best!
+
+>>>>>>> Stashed changes
